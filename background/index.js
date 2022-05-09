794f1cbd9727e3c8dd1cb0231dcbd5202992d4b1
Fariza Kubanova <farizatkubanova@gmail.com>
2022-05-09 20:55:14 +0100

    добавила switch

--- background/index.js
+++ background/index.js
@@ -25,6 +25,25 @@ function changeGreen() {
 }
 */
 
+
+
+
+
+
+/*
+let button=document.querySelector('.dropdown-menu')
+button.onclick=changeColor
+function changeColor() {
+switch (document.querySelector('.dropdown').value){
+    case 'white': document.body.style.background='white';break;
+    case "yellow": document.body.style.background='yellow'; break;
+    case 'green': document.body.style.background='green'; break;
+    case 'blue': document.body.style.background='blue'; break;
+    case 'pink':document.body.style.background="pink"; break;
+}}*/
+
+
+
 let button=document.querySelector('.dropdown-menu')
 button.onclick=changeColor
 function changeColor() {
@@ -42,5 +61,4 @@ function changeColor() {
     }
     if (document.querySelector('.dropdown').value=="green") {
         document.body.style.background='green';
-    }
-}
+    }}
